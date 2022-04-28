9d5332a38faa58344cc0cbc20e0dd845e143b6b6
MARIUS MOCKUS <mariusmockus93@gmail.com>
2022-04-28 21:02:41 +0300

    last commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hi!')
+
+const express = require('express');
+
+//import express from 'express'  galetu ir taip rasyti, kad nebutu tu trieju taskeliu
+const PORT = 9000;
+
+const app = express();
+
+app.listen(PORT, () => {
+    console.log('labas, serveris veikia!');
+});
+
+app.get('/', (request, response) => {
+    console.log('ivyko uzklausa')
+    response.send("Hello world!");
+});
