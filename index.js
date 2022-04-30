0daeaa94b7556523fb3dd68a418888cd1651b4ab
MARIUS MOCKUS <mariusmockus93@gmail.com>
2022-04-30 12:10:53 +0300

    Express uzduotis #2 Counter

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
+/* 1 uzduotis laiko
 const express = require('express');
 const app = express();
 
-app.listen(9000, () => {
+app.listen(9000, () => { 
     console.log('serveris paleistas!');
 });
 
@@ -11,4 +12,20 @@ app.get('/time', (request, response) => {
     const dateString = date.toLocaleDateString();
     const timeString = date.toLocaleTimeString();
     response.send(`${dateString} ${timeString}`);
+}); */
+
+// 2 uzduotis: Counter
+
+const express = require('express');
+const app = express();
+
+let count = 0;
+
+app.listen(9000, () => {
+    console.log('serveris paleistas!');
+});
+
+app.get('/counter', (request, response) => {
+    count++;
+    response.send(`Užklausų skaičius: ${count}`);
 });
